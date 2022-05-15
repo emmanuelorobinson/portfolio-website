85bf95903848910e3d9f3c1e6295b9ebb2e1a836
Emmanuel Robinson <emmanuelorobinson@github.com>
2022-05-15 09:31:15 +0400

    change in colors and hero text

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="hero-container ">
         <h1>Hello, my name is</h1>
         <h2>Emmanuel Robinson.</h2>
-        <h3>{"Eat, sleep, code, repeat."}</h3>
+        <h3>{"I like building cool things."}</h3>
         <p>
           {
             "I'm currently a Computer Science and Business Infromation Systems Double Major Student at Murdoch University. I love learning new stack/technology. I focus on understanding the most I can in different technologies."
