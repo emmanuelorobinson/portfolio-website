42b40335bc06bc7d6a20033265815809bd83eb21
Emmanuel Robinson <52470812+emmanuelorobinson@users.noreply.github.com>
2022-07-05 19:45:25 +0000

    still playing around with meta tags

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -8,8 +8,7 @@ import Head from "next/head";
 
 const Landing = () => {
   const meta = {
-    title:
-      "Emmanuel Robinson",
+    title: "Emmanuel Robinson",
     description: `Emmanuel Robinson is a software engineer, computer science and business information systems student at Murdoch University.`,
     image: "/assets/avatar.png",
     type: "website",
@@ -26,14 +25,10 @@ const Landing = () => {
           content="width=device-width, initial-scale=1"
         ></meta>
         <meta property="og:type" content={meta.type} />
-        <meta property="author" content="Emmanuel Robinson" />
-        <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
-        {meta.date && (
-          <meta property="article:published_time" content={meta.date} />
-        )}
+        <meta property="author" content="Emmanuel Robinson" />
       </Head>
       <div className="display-flex ">
         <Header />
