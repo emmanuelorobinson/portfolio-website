c6c971927d93f912c808d707a5e05f4673a774b8
Emmanuel Robinson <52470812+emmanuelorobinson@users.noreply.github.com>
2022-07-05 19:18:08 +0000

    changes to resume section

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
+import Head from "next/head";
 
 const resume = () => {
   return (
     <div>
+      <Head>
+        <title>Resume</title>
+        <meta content="Emmanuel Robinson's resume" name="description" />
+      </Head>
       <Header />
       <div className="resume_container">
         <object
