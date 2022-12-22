14f2ec5631843531dc51519d395b3d577f57cca5
emmanuelorobinson <manuelejeay@gmail.com>
2022-12-22 22:11:56 +0400

    Design overhaul

--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -7,7 +7,7 @@ const Contacts = () => {
         <h1>Get In Touch</h1>
         <p>
           {
-            "Although I’m not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!"
+            "My inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!"
           }
         </p>
 
